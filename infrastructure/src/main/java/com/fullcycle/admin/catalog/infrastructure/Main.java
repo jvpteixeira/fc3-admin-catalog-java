f26e080cd20aac514e02fbb12fa4dbb3ee251a5d
João Victor Teixeira <48188729+JoaoVictorPereiraTeixeira@users.noreply.github.com>
2022-06-30 22:08:37 -0300

    feat: injeting gateway implementation in use cases

--- infrastructure/src/main/java/com/fullcycle/admin/catalog/infrastructure/Main.java
+++ infrastructure/src/main/java/com/fullcycle/admin/catalog/infrastructure/Main.java
@@ -1,8 +1,17 @@
 package com.fullcycle.admin.catalog.infrastructure;
 
+import com.fullcycle.admin.catalog.application.category.create.CreateCategoryUseCase;
+import com.fullcycle.admin.catalog.application.category.delete.DeleteCategoryUseCase;
+import com.fullcycle.admin.catalog.application.category.retrieve.get.GetCategoryByIdUseCase;
+import com.fullcycle.admin.catalog.application.category.retrieve.list.ListCategoriesUseCase;
+import com.fullcycle.admin.catalog.application.category.update.UpdateCategoryUseCase;
 import com.fullcycle.admin.catalog.infrastructure.configuration.WebServerConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.sql.init.dependency.DependsOnDatabaseInitialization;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.AbstractEnvironment;
 
 @SpringBootApplication
@@ -12,4 +21,20 @@ public class Main {
         System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development");
         SpringApplication.run(WebServerConfig.class, args);
     }
+
+    @Bean
+    @DependsOnDatabaseInitialization
+    ApplicationRunner runner(
+            @Autowired CreateCategoryUseCase a,
+            @Autowired UpdateCategoryUseCase b,
+            @Autowired ListCategoriesUseCase c,
+            @Autowired DeleteCategoryUseCase d,
+            @Autowired GetCategoryByIdUseCase e
+    ){
+        return args -> {
+
+        };
+    }
+
+
 }
